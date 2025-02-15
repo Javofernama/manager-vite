f997b202861e0aa161cd3b359515155ca79c7691
Javier Fernandez <Javier.Fernandez@mejoradigital.cl>
2025-02-15 17:27:26 -0300

    fix: fixing prod envs

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
   server: {
-    allowedHosts: ["managerlite.mejoradigital.net"],
+    allowedHosts: ["managerlite.mejoradigital.net", "backendlite.mejoradigital.net"],
   }
 })
